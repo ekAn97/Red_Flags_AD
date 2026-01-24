74c022ad10e48c69bc095e122a5d0843e93576b4
ekAn97 <156143664+ekAn97@users.noreply.github.com>
2026-01-24 21:17:48 +0200

    Update main.py

--- api/main.py
+++ api/main.py
@@ -25,7 +25,7 @@ origins = [
     "http://127.0.0.1",
     "http://localhost:7274",
     "http://192.168.6.123:7274",
-    "https://redflags.iee.ihu.gr",
+    "https://redflags.iee.ihu.gr/",
 ]
 app.add_middleware(
     CORSMiddleware,
@@ -271,6 +271,7 @@ async def shutdown_event():
     db.close()
 
 # Run with: uvicorn main:app --host 0.0.0.0 --port 8000
+
 
 
 
